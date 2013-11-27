073930c41f138573faf1b55a71680f5bf5cb6319
Elmer <elmerwu@gmail.com>
2013-11-27 12:59:53 -0800

    add tests to test helper method for title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,7 +13,12 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
@@ -26,7 +31,12 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} | Help")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have custom title" do
+      visit '/static_pages/help'
+      expect(page).not_to have_title("| Help")
     end
   end
 
@@ -39,7 +49,12 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | About")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have custom title" do
+      visit '/static_pages/about'
+      expect(page).not_to have_title("| About")
     end
   end
 
@@ -52,7 +67,12 @@ describe "Static pages" do
 
     it "should have the right title" do
       visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title} | Contact")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have custom title" do
+      visit '/static_pages/contact'
+      expect(page).not_to have_title("| Contact")
     end
   end
 end
