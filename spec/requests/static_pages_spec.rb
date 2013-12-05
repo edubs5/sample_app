f5597b8fb89f91f754676d9f928a6cb741b95aba
Elmer <elmerwu@gmail.com>
2013-12-04 20:03:59 -0800

    change tests to test route instead of hard coded url address

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,18 +6,18 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+    it "should have the h1 'Sample App'" do
+      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("#{base_title}")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('| Home')
     end
   end
@@ -25,17 +25,17 @@ describe "Static pages" do
   describe "Help page" do
 
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
 
     it "should have the right title" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{base_title}")
     end
 
     it "should not have custom title" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).not_to have_title("| Help")
     end
   end
@@ -43,17 +43,17 @@ describe "Static pages" do
   describe "About page" do
 
     it "should have the content 'About Us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
 
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("#{base_title}")
     end
 
     it "should not have custom title" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).not_to have_title("| About")
     end
   end
@@ -61,17 +61,17 @@ describe "Static pages" do
   describe "Contact page" do
 
     it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
 
     it "should have the right title" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("#{base_title}")
     end
 
     it "should not have custom title" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).not_to have_title("| Contact")
     end
   end
