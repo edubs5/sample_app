3208571e9d6c1b7ce54cdb7b3ebaa076ae624726
Elmer <elmerwu@gmail.com>
2013-12-06 16:52:04 -0800

    Add spec helper to test full_title method. Add test for links. Modify test to have_content instead of have_title(full_title('xyz')), tests did not pass with the later case since the pages did not have titles..?

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -59,4 +59,21 @@ describe "Static pages" do
     it_should_behave_like "all static pages"
     it { should_not have_title('| Contact') }
   end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_content('About Us')
+    click_link "Help"
+    expect(page).to have_content('Help')
+    click_link "Contact"
+    expect(page).to have_content('Contact')
+    click_link "Home"
+    expect(page).to have_content('Home')
+    click_link "Sign up now!"
+    expect(page).to have_content('Sign up')
+    click_link "sample app"
+    expect(page).to have_content('sample app')
+  end
 end
+
